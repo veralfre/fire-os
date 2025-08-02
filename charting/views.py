30076836de20452a7211fa715e984869236122d7
veralfre <veralfre@hotmail.it>
2025-08-02 16:26:12 +0200

    End of the day

--- charting/views.py
+++ charting/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from accounting.models import Account, Transaction
 from django.db.models import Sum
 import json
-from collections import defaultdict
-from django.utils.timezone import make_aware
-import datetime
-# Create your views here.
+from django.core.paginator import Paginator
+
+def transactions(request):
+    transaction_list = Transaction.objects.all().order_by('-date')
+    paginator = Paginator(transaction_list, 25)  # 25 transactions per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'charting/transactions.html', {'page_obj': page_obj})
 
 def index(request):
 
@@ -32,9 +38,17 @@ def index(request):
         for month in months:
             progression.append(account.end_of_month_balance(int(month.split('-')[0]), 
                                                             int(month.split('-')[1])))
-            progression_core.append(account.end_of_month_balance(int(month.split('-')[0]), 
-                                                                int(month.split('-')[1]), 
-                                                                tags=['CORE']))
+            # We get all transaction in the year/month and also filter by CORE tag
+            # This is to get the CORE progression
+            core_qs = account.transactions.filter(date__month=int(month.split('-')[1]), 
+                                                  date__year=int(month.split('-')[0]), 
+                                                  transaction_tags__name='CORE',
+                                                  transaction_type__in=['debit'])
+            
+            account_core_sum = core_qs.aggregate(sum=Sum('amount'))['sum'] or 0
+            progression_core.append(float(abs(account_core_sum)))
+    
+
         datasets.append({
             'label': account.name,
             'data': progression,
@@ -50,7 +64,7 @@ def index(request):
         'labels': months,
         'datasets': datasets
     }
-    
+
     core_chart_data = {
         'labels': months,
         'datasets': datasets_core
@@ -58,6 +72,6 @@ def index(request):
 
     return render(request, 'charting/index.html', {
         'chart_data': json.dumps(chart_data),
-        'core_chart_data': json.dumps(core_chart_data)
+        'core_chart_data': json.dumps(core_chart_data),
     })
 
