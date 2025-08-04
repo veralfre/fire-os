4cf5af9b7e2e3130a58317a3f018af2a9bbc9c00
veralfre <veralfre@hotmail.it>
2025-08-04 08:52:39 +0200

    Start drafting API

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 
-from . import views 
+from . import views, api_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('schema/', views.schema, name='schema'),
     path('upload_transactions/', views.upload_transactions, name='upload_transactions'),
+    path('api/average/', api_views.average, name='average')
 ]
